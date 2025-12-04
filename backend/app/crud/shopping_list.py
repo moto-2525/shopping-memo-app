ad55bca7a5e4c037b5cb661554900224cf078eff
dlvvlb <dlv.vlb8@gmail.com>
2025-12-04 13:28:10 +0900

    Update models_crud2

--- backend/app/crud/shopping_list.py
+++ backend/app/crud/shopping_list.py
@@ -5,13 +5,13 @@ from app.models.shopping_list import ShoppingList
 from app.schemas.shopping_list import ShoppingCreate, ShoppingUpdate
 
 
-# ユーザーごと一覧取得
-def get_items_by_user(db: Session, firebase_uid: str):
+# UID一覧取得
+def get_lists_by_uid(db: Session, firebase_uid: str):
     return db.query(ShoppingList).filter(ShoppingList.firebase_uid == firebase_uid).all()
 
 
 # 新規作成
-def create_item(db: Session, data: ShoppingCreate):
+def create_list(db: Session, data: ShoppingCreate):
     db_obj = ShoppingList(
         firebase_uid=data.firebase_uid,
         item=data.item,
@@ -26,13 +26,13 @@ def create_item(db: Session, data: ShoppingCreate):
 
 
 # 1件取得
-def get_item(db: Session, item_id: int):
+def get_list(db: Session, item_id: int):
     return db.query(ShoppingList).filter(ShoppingList.id == item_id).first()
 
 
 # 更新（PATCH）
-def update_item(db: Session, item_id: int, updates: ShoppingUpdate):
-    db_obj = get_item(db, item_id)
+def update_list(db: Session, item_id: int, updates: ShoppingUpdate):
+    db_obj = get_list(db, item_id)
 
     if not db_obj:
         raise HTTPException(
@@ -54,8 +54,26 @@ def update_item(db: Session, item_id: int, updates: ShoppingUpdate):
     return db_obj
 
 
+# is_checked  true/false 
+def toggle_done(db: Session, item_id: int):
+    db_obj = get_list(db, item_id)
+
+    if not db_obj:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Item not found"
+        )
+
+    db_obj.is_checked = not db_obj.is_checked
+
+    db.commit()
+    db.refresh(db_obj)
+    return db_obj
+
+
+
 # 削除
-def delete_item(db: Session, item_id: int):
+def delete_list(db: Session, item_id: int):
     db_obj = get_item(db, item_id)
 
     if not db_obj:
